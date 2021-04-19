0629766748ecfdb5007871c4f60e4da3bb5aa199
bslease <bslease@gmail.com>
2021-04-18 18:58:38 -0700

    implement ship movement

--- Space SHMUP Prototype/Assets/__Scripts/Hero.cs
+++ Space SHMUP Prototype/Assets/__Scripts/Hero.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    private void Awake()
     {
-        
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Give some pitch and roll to the ship when it moves...
+        // The ship is pointing along the y axis, on the xy plane
+        // if we move up or down the screen (vertically), rotate around the x axis (pitch)
+        // if we move left or right on the screen (horizontally), rotate around the y axis (roll) 
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
     }
 }
