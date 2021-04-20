cc51c5536b3f1946e410d34bd68893975c911a08
bslease <bslease@gmail.com>
2021-04-19 20:17:17 -0700

    begin enemy assets and implementation

--- Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
+++ Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
@@ -10,10 +10,15 @@ public class BoundsCheck : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public float radius = 1f; // of the thing you want to keep on the screen
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     private void Awake()
     {
@@ -24,28 +29,40 @@ public class BoundsCheck : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
 
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            offRight = true;
         }
 
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            offLeft = true;
         }
 
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            offUp = true;
         }
 
         if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            offDown = true;
         }
 
-        transform.position = pos;
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
     }
 
     private void OnDrawGizmos()
---
+++ Space SHMUP Prototype/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100; // points earned for destroying this
+
+    private BoundsCheck bndCheck;
+
+    public Vector3 pos
+    {
+        get
+        {
+            return this.transform.position;
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    private void Update()
+    {
+        Move();
+
+        if (bndCheck != null && bndCheck.offDown)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
